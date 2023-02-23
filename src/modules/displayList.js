6037a7a43f38a2ee4582d7dfe582d6f005c88f06
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-24 01:48:37 +0300

    Add check Icon

--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -5,12 +5,12 @@ export default (todoObjects) => todoObjects.map((todo) => {
   li.setAttribute('data-completed', todo.completed);
 
   li.innerHTML = todo.completed ? `
-  <i class='fa-regular fa-circle-check todo-completed fa-2x'></i>` : `
+  <div class='todo-completed'><i class='fa-regular fa-circle-check fa-2x'></i></div>` : `
   <div class='todo-check'><i class="fa-regular fa-square fa-2x"></i></div>  `;
   li.innerHTML += `
   <input type='text' class='todo-edit' value='${todo.description}'/>
-  <i class="fas fa-ellipsis-v todo-ellipsis fa-2x hide"></i>
-  <div class='todo-delete '><i class="fa-regular fa-trash-can fa-2x"></i></div>
+  <i class="fas fa-ellipsis-v todo-ellipsis fa-2x"></i>
+  <div class='todo-delete hide'><i class="fa-regular fa-trash-can fa-2x"></i></div>
   `;
   return li;
 });
